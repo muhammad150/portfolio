50945f3c0e102ba4abc5bad289f923ae5cd02031
muhammad150 <pktiger20@gmail.com>
2020-04-13 12:14:07 -0700

    initial commit

---
+++ jobs/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class job(models.Model):
+    # image = models.imageField(upload_t0='')
+    field = models.ImageField(max_length=256,upload_to=''),
+    summary = models.CharField(max_length=200)
