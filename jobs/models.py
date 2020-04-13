98d1eaec34da663d4aa545a9c90cc6f134b66d8b
muhammad150 <pktiger20@gmail.com>
2020-04-13 12:21:03 -0700

    models updated

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,4 @@ class job(models.Model):
     # image = models.imageField(upload_t0='')
     field = models.ImageField(max_length=256,upload_to=''),
     summary = models.CharField(max_length=200)
+    # this is new line
